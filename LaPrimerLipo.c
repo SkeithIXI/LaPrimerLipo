acd027b88413348cd21cc58f3ade7918889b1365
Santiago Ortiz Frias <skeith3550@gmail.com>
2026-04-14 10:39:14 -0700

    Update LaPrimerLipo.c
    Case 2 Agregado

--- LaPrimerLipo.c
+++ LaPrimerLipo.c
@@ -18,4 +18,17 @@ do
   puts("45,000        Abdomen o papada                        N/A ");
   puts("100,000    Abdomen,papada,brazos,esopalda       Fajas, medicamentos ");
   puts("140,000    Abdomen,papada,brazos,esopalda       Fajas, masajes linfaticos,
-                         muslos y axilas                 medicamentos");       
+                         muslos y axilas                 medicamentos");  
+break;
+case 2: puts("Cita agendada");
+		printf("Ingrese su nombre");
+		scanf("s%",nom);
+		printf("Ingrese su edad");
+		scanf("d%",&edad);
+		printf("Ingrese la cantidad a pagar");
+		scanf("d%",&precio);
+		if (precio<45000){
+			preintf("Precio menor de 45000 no valido");
+		}
+		system ("pause");
+		break;
